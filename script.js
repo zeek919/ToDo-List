a43ff7bc7d63104cee21d5ba6d5bfb944e870c2b
Zeek918 <zeek19988@wp.pl>
2019-10-16 20:45:36 +0200

    Fix bugs and first BEM style
    Bugs in 'nothingMessage' and 'main section' are fixed now. In css evry
    name of classes is changed to BEM (I hope it is good)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('button');
 const input = document.querySelector('input');
-const wraper = document.querySelector('.wraper');
+const wraper = document.querySelector('.section--wraper');
 const ul = wraper.querySelector('ul');
 
 const main = document.querySelector('main');
@@ -8,44 +8,59 @@ const main = document.querySelector('main');
 
 const nothingMessage = wraper.querySelector('div');
 let counter = {value: 0};
+let counterColor = {value: 0};
 
-const validate = function() {
-    if(input.value.length > 10)
+const validateShowMessage = function() {
+    if(counter.value < 1)
     {
-        main.style.backgroundColor = 'red';
-        main.style.animationName = 'tooLongValue';
-        main.style.animationDuration = '2s';
-        main.style.animationFillMode = 'forwards';
-
-        input.value = '';
-        input.placeholder = 'ZA DUŻO ZNAKÓW!';
+        nothingMessage.style.display = 'inline';
     }
     else
     {
-        generateElement('li');
+        nothingMessage.style.display = 'none';
+    }
+}
+
+const validateColor = function() {
+
+    if(input.value.length > 10)
+    {
+        main.classList.remove('main--correctValue');
+        main.classList.add('main--wrongValue');
+
+        input.value = '';
+        input.placeholder = 'ZA DUŻO ZNAKÓW!';
+        counterColor.value++;
+    }
+    else
+    {
+        if(counterColor.value>=1) {
+            main.classList.remove('main--wrongValue');
+            main.classList.add('main--correctValue');
+        }
 
         input.placeholder = 'Wpisz coś...';
-        main.style.backgroundColor = '#fcfcfc';
-
-        main.style.animationName = 'correctValue';
-        main.style.animationDuration = '2s';
-        main.style.animationFillMode = 'forwards';
+        generateElement('li');
+    
     }
 }
 
 const generateButton = function() 
 {
     const createButton = document.createElement('button');
-    createButton.style.cssFloat = 'right';
+    
     createButton.innerText = 'USUŃ';
     createButton.classList.add("li__button--delete");
 
     createButton.addEventListener('click', function()
     {
         console.log(createButton.parentNode);
+        counter.value--;
+
         const parentToDelete = createButton.parentNode;
 
         parentToDelete.parentNode.removeChild(parentToDelete);
+        validateShowMessage();
     });
 
     return createButton;
@@ -54,53 +69,22 @@ const generateButton = function()
 const generateElement = function(htmlElement)
 {
     const createElement = document.createElement(htmlElement);
-    createElement.classList.add('element');
+    createElement.classList.add('section__li--task');
     createElement.innerText = input.value;
     createElement.id = `count_${counter.value}`;
 
     var elChildButton = ul.appendChild(createElement);
-    
-
-    if(counter>=0)
-    {
-        nothingMessage.style.display = 'none';
-    }
+    counter.value++;
 
     elChildButton.appendChild(generateButton());
-    counter.value++;
+    validateShowMessage();
+
 }
+
+
 
 
 button.addEventListener('click', function(){
-    validate();
+    validateColor();
 });
 
-
-
-
-
-/*  WERSJA I - DZIALA
-
-
-const generateElement = function(htmlElement)
-{
-    const createElement = document.createElement(htmlElement);
-    createElement.classList.add('element');
-    createElement.innerText = input.value;
-
-    var elChildButton = ul.appendChild(createElement);
-    
-
-    if(counter>=0)
-    {
-        nothingMessage.style.display = 'none';
-    }
-
-
-    const crButton = document.createElement('button');
-    crButton.style.cssFloat = 'right';
-    crButton.innerText = 'USUŃ';
-
-    elChildButton.appendChild(crButton);
-}
-*/
